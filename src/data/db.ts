922048f632efa3b1f1f710aa37b3fcb8a05e88f9
starlightroad <ortizge92@gmail.com>
2025-02-01 15:33:04 -0500

    Update the utility data routes

--- src/data/db.ts
+++ src/data/db.ts
@@ -1,3 +1,4 @@
+import { ROUTES } from "../lib/constants";
 import type { Utility } from "../lib/definitions";
 
 export const utilityData: Utility[] = [
@@ -6,13 +7,13 @@ export const utilityData: Utility[] = [
     label: "Time Duration Calculator",
     description:
       "Calculate the duration between two times in years, months, hours, and minutes.",
-    href: "/time-duration-calculator",
+    href: ROUTES.TIME_DURATION_CALLCULATOR,
   },
   {
     id: "ul-2",
     label: "Random ID Generator",
     description:
       "Generate pseudo-random numbers using v4 UUIDs (Universally Unique Identifiers).",
-    href: "/random-id-generator",
+    href: ROUTES.RANDOM_ID_GENERATOR,
   },
 ];
