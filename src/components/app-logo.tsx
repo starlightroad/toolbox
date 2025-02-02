dbd56d2c5753a9f8e08e8acd52635c58d8a8e36e
starlightroad <ortizge92@gmail.com>
2025-02-01 19:24:17 -0500

    Replace app logo

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router";
-import { ContainerIcon } from "lucide-react";
 import { ROUTES } from "../lib/constants";
 
 export default function AppLogo() {
   return (
     <Link to={ROUTES.HOME} className="flex items-center gap-2 text-white">
-      <ContainerIcon size={20} />
+      <img src="/favicon-32x32.png" alt="Website logo" width={20} height={20} />
       <span className="text-sm font-medium">toolbox</span>
     </Link>
   );
