73c47011a3be4b16782f4e70c604c39919316949
starlightroad <ortizge92@gmail.com>
2026-03-06 16:53:11 -0500

    Remove unnecessary escape characters

--- src/features/time-duration-calculator/lib/constants.ts
+++ src/features/time-duration-calculator/lib/constants.ts
@@ -1,16 +1,8 @@
 export const INPUT_TIME_REGEX =
-  /^(?:(?:(?:\d){1,2}[\-|\/]){2}[0-9]{4}\s)?(?:(?:(?:[1-9]|[1][0-2])\:(?:[0-5][0-9])\s(?:am|AM|pm|PM))|(?:(?:[0-1][0-9]|[2][0-3])\:[0-5][0-9]))$/;
+  /^(?:(?:(?:\d){1,2}[-|/]){2}[0-9]{4}\s)?(?:(?:(?:[1-9]|[1][0-2]):(?:[0-5][0-9])\s(?:am|AM|pm|PM))|(?:(?:[0-1][0-9]|[2][0-3]):[0-5][0-9]))$/;
 
 export const TIME = {
-  MILLISECONDS: {
-    PER_SECOND: 1000,
-  },
-  SECONDS: {
-    PER_MINUTE: 60,
-    PER_HOUR: 60 * 60,
-    PER_DAY: 24 * 60 * 60,
-  },
-  HOURS: {
-    PER_DAY: 24,
-  },
+  MILLISECONDS: { PER_SECOND: 1000 },
+  SECONDS: { PER_MINUTE: 60, PER_HOUR: 60 * 60, PER_DAY: 24 * 60 * 60 },
+  HOURS: { PER_DAY: 24 },
 };
